2f3bd1f48364154b96291f9b7df151455744024b
Hamza Hayech <hamza.ayech@uteek.net>
2019-11-11 14:17:15 +0100

    first commit

---
+++ resources/js/App.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Route, Redirect, Switch, withRouter } from "react-router-dom";
+import HomePage from "./pages/home-page/HomePage";
+import Navbar from "./components/navbar/Navbar";
+import SigninPage from "./pages/signin-page/SigninPage";
+
+import SignUpPage from "./pages/signup-page/SignUpPage";
+import { logout } from "./actions/auth-actions/actions";
+import PostsPage from "./pages/posts-page/PostsPage";
+
+const App = props => {
+  return (
+    <div className={"app"}>
+      <Navbar
+        user={props.user}
+        isLoggedIn={props.isLoggedIn}
+        logout={props.logout}
+      />
+      <Switch location={props.history.location}>
+        <Route exact path={"/"} component={HomePage} />
+        <AuthRoute
+          authenticated={props.isLoggedIn}
+          path="/posts"
+          component={PostsPage}
+        />
+        <Route
+          authenticated={props.isLoggedIn}
+          path="/signup"
+          component={SignUpPage}
+        />
+        <Route
+          authenticated={props.isLoggedIn}
+          path="/signin"
+          component={SigninPage}
+        />
+        />
+      </Switch>
+    </div>
+  );
+};
+function AuthRoute({ component: Component, authenticated, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      exact
+      render={props =>
+        authenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: "/signin", state: { from: props.location } }}
+          />
+        )
+      }
+    />
+  );
+}
+
+
+const mapStateToProps = reduxStore => {
+  return {
+    isLoggedIn: reduxStore.authReducer.isLoggedIn,
+    user: reduxStore.authReducer.user,
+    isLoadingUser: reduxStore.authReducer.isLoadingUser
+  };
+};
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    { logout }
+  )(App)
+);
